17186e42d8d119c1a004dd2c9fc96ae03e4d23d0
Mamurjon-mnz <mamur.satimov.85@mail.ru>
2025-08-13 16:01:07 +0500

    Update MS hometask-3.py

--- lesson-3/homework/MS hometask-3.py
+++ lesson-3/homework/MS hometask-3.py
@@ -22,8 +22,9 @@ print(yangi_ruyxat)
 #4. Ro'yxatni kortejga aylantirish
 #Sevimli beshta filmingiz ro'yxatini yarating va uni kortejga aylantiring.
 
-ruyxat='Olma','Banan','Uzum','Apelsin','Nok'
-ruyxat
+ls=['Olma','Banan','Uzum','Apelsin','Nok']
+ls=tuple(ls)
+print(ls)
 #5.Ro'yxatda element borligini tekshirish
 #Shaharlar ro'yxati berilgan. Unda "Paris" bor-yo‘qligini tekshiring va natijani chop eting.
 
@@ -52,7 +53,7 @@ print(sonlar)
 
 ruyxat=(1,2,3,4,5,6,7,8,9,10)
 ruyxat[2:7]
-
+print(ruyxat)
 #9. Ro'yxatda nechta marta uchrashini hisoblash
 #Ranglar ro'yxatini yarating va unda "blue" so‘zi nechta marta uchrashini hisoblang.
 
@@ -60,45 +61,44 @@ ruyxat[2:7]
 
 list_rang=('blue','white', 'blue', 'red','yellow','green','blue')
 soni=list_rang.count('blue')
-soni
+print(soni)
 #10. Kortejda element indeksini topish
 #Hayvonlar korteji berilgan. Undan "lion" elementining indeksini toping.
 
 hayvonlar=("Bear","Fox", "Tiger", "Dog", "Monkey", "Cow", "Lion")
 indeks=hayvonlar.index("Lion")
-indeks
+print(indeks)
 #11. Ikki kortejni birlashtirish
 #Ikki raqamlar kortejini yarating va ularni bitta kortejga birlashtiring.
 
 ruyxat1=("Akmal", "Sarvar")
 ruyxat2=(1,3,7)
 jamlanma=ruyxat1+ruyxat2
-jamlanma
+print(jamlanma)
 #12. Ro'yxat va kortej uzunligini topish
 #Berilgan ro'yxat va kortej uzunligini toping va chop eting.
 
 ruyxat=('Nodir',33, "Siroj","Humoyun","Jamshid",66)
 uzunlik=len(ruyxat)
-uzunlik
+print(uzunlik)
 #13. Kortejni ro'yxatga aylantirish
 #Besh raqamdan iborat kortej yarating va uni ro'yxatga aylantiring.
 
 a=(1,2,3,4,5)
 a=list(a)
-a
+print(a)
 #14. Kortejdagi maksimal va minimal qiymatlarni topish
 #Berilgan raqamlar kortejidan eng katta va eng kichik qiymatlarni topib, chop eting
 
 b=(15,8,9,23,75,43,89,14,5,32,88,98,56)
 minimal=min(b)
-minimal
-
+print(minimal)
 
 maksimal=max(b)
-maksimal
+print(maksimal)
 #15. Kortejni teskari tartibda chiqarish
 #So‘zlardan iborat kortej yarating va uni teskari tartibda chop eting.
 
 d=("olma", "suv", "asal", "lion", "moliya")
 f=d[::-1]
-f
+print(f)
